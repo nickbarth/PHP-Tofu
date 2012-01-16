cb5e13452c08797bed9983463f78e2bebbb62797
Nick Barth <nickbarth@live.com>
2012-01-15 18:47:14 -0700

    updated layout

--- app/views/layouts/apples.php
+++ app/views/layouts/apples.php
@@ -2,17 +2,17 @@
 <html lang="en">
 	<head>
 		<meta charset="UTF-8">
-		<title>Tofu Manager</title>
+		<title>Apple Manager</title>
 		<link rel="StyleSheet" href="{{ STYLE }}/reset.css" type="text/css" media="screen" />
 		<link rel="StyleSheet" href="{{ STYLE }}/style.css" type="text/css" media="screen" />
 		<script type="text/javascript" src="{{ JS }}/main.js" ></script>
 	</head>
 	<body>
-	<h1>Tofu Manager</h1>
+	<h1>Apple Manager</h1>
     <a href="<?php print BASEURL ?>/">Home</a>
-    <a href="<?php print BASEURL ?>/create/">Create Tofu</a>
+    <a href="<?php print BASEURL ?>/create/">Create Apple</a>
     <br/>
     <?php print $contents ?>
-    Tofu Manager &copy; 2011 by Nick Barth
+    Apple Manager &copy; 2011 by Nick Barth
 	</body>
 </html>
