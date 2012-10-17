cd6e71469d7d6da1210b6968ea9ee85f9e9c8be5
Nick Barth <nickbarth@live.com>
2012-10-17 15:29:40 -0600

    fixed indentations

--- app/helpers/template.php
+++ app/helpers/template.php
@@ -1,35 +1,35 @@
 <?php
-  $templates = array('<<IMAGES>>'  => BASEURL.'/style/images',
-                     '<<CSS>>'     => BASEURL.'/style',
-                     '<<JS>>'      => BASEURL.'/style/js',
-                     '<<BASEURL>>' => BASEURL);
+	$templates = array(
+		'<<IMAGES>>'  => BASEURL.'/style/images',
+		'<<CSS>>'     => BASEURL.'/style',
+		'<<JS>>'      => BASEURL.'/style/js',
+		'<<BASEURL>>' => BASEURL);
 
-  function post($var, $object)
-  {
-    // Output and Escape a POST Variable if Set
-    if (isset($_POST[$object][$var]))
-      print escape($_POST[$object][$var]);
-  }
+	function post($var, $object)
+	{
+		// Output and Escape a POST Variable if Set
+		if (isset($_POST[$object][$var]))
+			print escape($_POST[$object][$var]);
+	}
 
-  function error($object, $var)
-  {
-    // Output and Escape a POST Variable if Set
-    if (isset($object->errors[$var]))
-      print $object->errors[$var][0];
-  }
+	function error($object, $var)
+	{
+		// Output and Escape a POST Variable if Set
+		if (isset($object->errors[$var]))
+			print $object->errors[$var][0];
+	}
 
-  function postf(&$var, $post)
-  {
-    // Output User POST Input if set or Default Variable
-    if (isset($_POST[$post]))
-      print escape($_POST[$post]);
-    elseif (!empty($var))
-      print escape($var);
-  }
+	function postf(&$var, $post)
+	{
+		// Output User POST Input if set or Default Variable
+		if (isset($_POST[$post]))
+			print escape($_POST[$post]);
+		elseif (!empty($var))
+			print escape($var);
+	}
 
-  // sprintf %s
-  function escape()
-  {
-    // Short HTML Entities Call
-    return call_user_func_array('htmlentities', func_get_args());
-  }
+	function escape()
+	{
+		// Short HTML Entities Call
+		return call_user_func_array('htmlentities', func_get_args());
+	}
