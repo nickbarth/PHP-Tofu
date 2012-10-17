ceba4948083c16f520c64a03bba2d9ca76c497a9
Nick Barth <nickbarth@live.com>
2012-10-17 14:56:46 -0600

    fixed indentations

--- app/controllers/apples.php
+++ app/controllers/apples.php
@@ -16,20 +16,20 @@
 		public function create()
 		{
 			// Create an Tofu Page
-      $this->apple = Apple::create($_POST['apple']);
+			$this->apple = Apple::create($_POST['apple']);
 		}
 		
 		public function update($appleID = null)
 		{
 			// Update an Tofu
 			$this->apple = Apple::find($appleID);
-      $this->apple->update($_POST['apple']);
+			$this->apple->update($_POST['apple']);
 		}
 		
 		public function remove($appleID = null)
 		{
 			// Confirm and Delete an Tofu
 			$this->apple = Apple::find($appleID);
-      $this->apple->remove($_POST['apple']);
+			$this->apple->remove($_POST['apple']);
 		}
 	}
